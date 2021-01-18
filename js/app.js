0b769fe7f008a83081fb805bce06424c2b4ebe8f
AStevens323 <71614086+AStevens323@users.noreply.github.com>
2021-01-18 12:16:59 -0500

    Getting random number array done.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let qwertyVariable = document.getElementById('qwerty');
 let phraseVariable = document.getElementById('phrase');
-const startGame = document.querySelector('.btn_reset');
+const startGame = document.querySelector('.btn__reset');
 let missedQuestion = 0;
 
 let phrases = ['I', 'love', 'creating', 'coding', 'projects'];
@@ -8,6 +8,21 @@ let phrases = ['I', 'love', 'creating', 'coding', 'projects'];
 
 startGame.addEventListener('click', () => {
     const displayOverlay = document.getElementById('overlay');
-    displayOverlay.style.display=none;
+    displayOverlay.style.display='none';
 
 });
+
+const getRandomPhraseArray = (arr) => {
+   /*Gets random number*/
+   let randomNumber = Math.floor(Math.random() * phrases.length);
+   return arr[randomNumber];
+
+}
+
+const addPhraseToDisplay = (arr) => {
+    for(i=0; i<arr.length; i++) {
+
+
+    }
+
+}
