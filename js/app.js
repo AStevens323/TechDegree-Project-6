1189b3833f8b994f33bcb5cc9bd176140c0ef167
AStevens323 <71614086+AStevens323@users.noreply.github.com>
2021-01-18 10:06:32 -0500

    Trying to get .addEventListener working

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 let qwertyVariable = document.getElementById('qwerty');
 let phraseVariable = document.getElementById('phrase');
-const btnStart = document.getElementsByClassName('btn_reset');
-const displayOverlay = document.getElementById('overlay');
-
-let missedQuestion = i=0;
+const startGame = document.querySelector('.btn_reset');
+let missedQuestion = 0;
 
 let phrases = ['I', 'love', 'creating', 'coding', 'projects'];
 
-btnStart.addEventListener('click', () => {
-    displayOverlay.display=none;
+
+startGame.addEventListener('click', () => {
+    const displayOverlay = document.getElementById('overlay');
+    displayOverlay.style.display=none;
 
 });
