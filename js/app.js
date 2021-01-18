0f4486c70ebc7af66e64d8b694d35160992179df
AStevens323 <71614086+AStevens323@users.noreply.github.com>
2021-01-18 09:18:09 -0500

    Variables added to app.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let qwertyVariable = document.getElementById('qwerty');
+let phraseVariable = document.getElementById('phrase');
+const btnStart = document.getElementsByClassName('btn_reset');
+
+let missedQuestion = i=0;
+
+let phrases = ['I', 'love', 'creating', 'coding', 'projects'];
+
+btnStart.addEventListener('click', () => {
+
+
+});
