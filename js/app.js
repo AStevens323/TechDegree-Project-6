1aa4e3d08104e669849cdbd52b1c6cf29a44e692
AStevens323 <71614086+AStevens323@users.noreply.github.com>
2021-01-18 09:30:37 -0500

    Overlay Display Hidden Trial 1

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 let qwertyVariable = document.getElementById('qwerty');
 let phraseVariable = document.getElementById('phrase');
 const btnStart = document.getElementsByClassName('btn_reset');
+const displayOverlay = document.getElementById('overlay');
 
 let missedQuestion = i=0;
 
 let phrases = ['I', 'love', 'creating', 'coding', 'projects'];
 
 btnStart.addEventListener('click', () => {
-
+    displayOverlay.display=none;
 
 });
